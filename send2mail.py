885839f8ca0efd62f43148857ac72eb6508119fa
Marko <m.stumberger@bbw.nazvezi.si>
2017-04-02 00:00:46 +0200

    edited imports so pyinstaller can freeze application, uncommented exception

--- send2mail.py
+++ send2mail.py
@@ -4,42 +4,40 @@ import os
 # Import smtp lib for the actual sending function
 import smtplib
 
-
 # Import the email modules we'll need
-import email
-import email.mime.application
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 # Function send mail
 def sendMail(server, port, sfrom, password, sto, subject, body, filepath):
     # Create a text/plain message
-    msg = email.mime.Multipart.MIMEMultipart()
+    msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sfrom
     msg['To'] = sto
 
     # The main body is just another attachment
-    msg.attach(email.mime.Text.MIMEText(body))
+    msg.attach(MIMEText(body))
 
     # PDF attachment
     if filepath:
         path, filename = os.path.split(filepath)
         fp = open(os.path.join(path, filename), 'rb')
-        att = email.mime.application.MIMEApplication(fp.read(), _subtype="pdf")
+        att = MIMEApplication(fp.read(), _subtype="pdf")
         fp.close()
         att.add_header('Content-Disposition', 'attachment; filename=%s' % filename)
         msg.attach(att)
 
     # send via Gmail server
 
-    #try:
-    s = smtplib.SMTP(server, port)
-    s.starttls()
-    s.login(sfrom, password)
-    s.sendmail(sfrom, sto, msg.as_string())
-    s.quit()
-    ret="Successfully sent email to "+sto
-    return ret
-    #except Exception as e:
-    #    print e
-    #    return "Error: unable to send email"
+    try:
+        s = smtplib.SMTP(server, port)
+        s.starttls()
+        s.login(sfrom, password)
+        s.sendmail(sfrom, sto, msg.as_string())
+        s.quit()
+        return "Successfully sent email to {}".format(sto)
+    except Exception as e:
+        return "Error: unable to send email. {}".format(e)
